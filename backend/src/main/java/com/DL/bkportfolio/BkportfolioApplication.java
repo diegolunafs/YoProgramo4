8631fa63063739b71476b79051d9461284d19068
diegolunafs <diegoluna17.dev@gmail.com>
2023-05-13 19:25:52 -0300

    Implementación del backend

---
+++ backend/src/main/java/com/DL/bkportfolio/BkportfolioApplication.java
@@ -0,0 +1,13 @@
+package com.DL.bkportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkportfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BkportfolioApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/DL/bkportfolio/BkportfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DL.bkportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkportfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
